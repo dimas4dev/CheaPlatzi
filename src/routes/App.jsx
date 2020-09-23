60446db254e4c55a3882a0bd63daff682b6ae396
HarpeBlue <hspe2507@gmail.com>
2020-09-22 20:48:38 -0500

    [FIX] Fixing repository configuration

---
+++ src/routes/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const App = () => (
+  <h1>Hola mundo</h1>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,6 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./public/index.html",
       filename: "./index.html",
-      favicon: "./public/favicon.png",
     }),
     new MiniCssExtractPlugin({
       filename: "assets/[name].css",
